8555c08a79869bab6d64b80855fca6741906c349
kostiantyn <opezdalya@gmail.com>
2023-07-19 14:01:52 +0000

    Commit

--- python-scripts/parcer_python_api.py
+++ python-scripts/parcer_python_api.py
@@ -8,15 +8,16 @@ api = Api(app)
 parcer = Parcers()
 postgres_api_cursor = ParcerPostgresAPI()
 
-class ParcerPythonAPI:
+class ParcerPythonAPI(Resource):
 
     def get(self):
+        
         print(postgres_api_cursor.get_items())
         return postgres_api_cursor.get_items()
-
+        
     def post(self, url):
         return parcer.ebayParcer("https://www.ebay.co.uk/b/Apple-Mobile-Smartphones/9355/bn_449685?LH_ItemCondition=1000%7C1500&rt=nc&_udlo=280&mag=1")
 
-api.add_resource(ParcerPythonAPI, "/parcer")
+api.add_resource(ParcerPythonAPI, "/all")
 if __name__ == "__main__":
     app.run()
