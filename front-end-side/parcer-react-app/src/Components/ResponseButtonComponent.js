fbc2fa2493c36377f79bdbeb691a1408e67e693f
kostiantyn <opezdalya@gmail.com>
2023-07-20 10:56:48 +0000

    Commit

--- front-end-side/parcer-react-app/src/Components/ResponseButtonComponent.js
+++ front-end-side/parcer-react-app/src/Components/ResponseButtonComponent.js
@@ -5,25 +5,26 @@ const Button = () => {
 
   const handleClick = () => {
     const url = 'http://localhost:5000/main';
-    const reload_url = 'https://8080/actuator/restart' 
+    const reload_url = 'http://localhost:8080/actuator/restart' 
     const requestData = {
       url: 'https://www.ebay.co.uk/b/Apple-Mobile-Smartphones/9355/bn_449685?LH_ItemCondition=1000%7C1500&rt=nc&_udlo=280&mag=1', 
     };
 
     axios.post(url, requestData)
       .then(response => {
-        console.log(response.data); 
+        console.log(response.data);
+
+        axios.post(reload_url)
+
+            .then(response =>{
+                console.log("Server has been reload \n", response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error)
+            }); 
       })
       .catch(error => {
         console.error('Error:', error);
-      });
-
-      axios.post(reload_url)
-      .then(response =>{
-        console.log("Server has been reload \n", response.data);
-      })
-      .catch(error => {
-        console.error('Error:', error)
       });
   };
 
--- src/main/java/com/example/parcerprojectkostiantyn/Controller/ReloadController.java
+++ src/main/java/com/example/parcerprojectkostiantyn/Controller/ReloadController.java
@@ -4,12 +4,14 @@ import com.example.parcerprojectkostiantyn.ParcerProjectKostiantynApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/actuator")
+@CrossOrigin
 public class ReloadController {
 
     private final ConfigurableApplicationContext context;
